3892abd56c129f80cc4e1588ab9f7e0f0fb369fe
poirotman <alexis.poirot19@gmail.com>
2022-03-14 09:08:45 +0100

    gestion cookie

--- TP3/connected.php
+++ TP3/connected.php
@@ -3,7 +3,8 @@
 $users = array(
 // login => password
 'riri' => 'fifi',
-'yoda' => 'maitrejedi' );
+'yoda' => 'maitrejedi',
+'admin' => 'rootAdmin');
 $login = "anonymous";
 $errorText = "";
 $successfullyLogged = false;
---
+++ TP3/index.php
@@ -0,0 +1,32 @@
+<?php
+    $DEFAULT ='style1';
+    if(isset($_GET['css'])){
+        if(isset($_COOKIE['Style'])){
+            unset($_COOKIE['Style']);
+        }
+        setcookie('Style', $_GET['css']);
+    }
+?>
+<head>
+    <meta charset="UTF-8">
+<?php 
+    if(isset($_GET['css'])){
+        echo '<link rel="stylesheet" href="'.$_GET['css'].'.css">';
+    }
+    elseif(isset($_COOKIE['Style'])){
+        echo '<link rel="stylesheet" href="'.$_COOKIE['Style'].'.css">';
+    }
+    else{
+        echo '<link rel="stylesheet" href="'.$DEFAULT.'.css">';
+    }
+?>
+</head>
+
+<form id="style_form" action="index.php" method="GET">
+    <select name="css">
+        <option value="style1">style1</option>
+        <option value="style2">style2</option>
+    </select>
+    <input type="submit" value="Appliquer" />
+</form>
+<button class='button'> Exemple du style </button> 
