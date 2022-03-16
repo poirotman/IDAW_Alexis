e533d4bf50c97e83b9eb6a67b9f6bab4e448b586
poirotman <alexis.poirot19@gmail.com>
2022-03-16 13:29:32 +0100

    Correction requete AJAX et réponse

--- TP4/addUser.php
+++ TP4/addUser.php
@@ -5,9 +5,9 @@
     $dbname='identifiant';
     $conn = new mysqli($servername, $username, $password,$dbname);
     if(isset($_POST['nom'])){
-        print_r($_POST);
-        
-        // $conn->query("INSERT INTO user (NOM,PRENOM,DATE,AIME_LE_COURS,REMARQUES)
-        //     VALUES ("$_POST()")
-    } 
+        $res=[];
+        $res['req']=true;
+        $res['nom']=$_POST['nom'];
+        echo json_encode( $res,0);
+      }
 ?>
