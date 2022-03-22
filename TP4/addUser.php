65702c6f3ca05176722d0fa8b89533be998f4401
poirotman <alexis.poirot19@gmail.com>
2022-03-22 11:59:24 +0100

    Amélioration edit et casi fini add ajax

--- TP4/addUser.php
+++ TP4/addUser.php
@@ -7,7 +7,31 @@
     if(isset($_POST['nom'])){
         $res=[];
         $res['req']=true;
-        $res['nom']=$_POST['nom'];
+        $sql="INSERT INTO user VALUES( NULL";
+        foreach($_POST as $key => $value){
+          if($key != 'id'){
+            if($key == 'date'){
+              if($value == ''){
+                $sql=$sql.", NULL";
+              }
+              else{
+                $timestamp = strtotime($value); 
+                $newDate = date("Y-m-d", $timestamp );
+                $sql=$sql.",'".$newDate."'";
+              } 
+            }else{
+              $sql=$sql.",'".$value."'";
+            }
+          }
+        }
+        $sql=$sql.")";
+        
+        $res['sql']=$sql;
+        $conn -> query($sql); 
+        $response = $conn -> query("SELECT ID FROM user ORDER BY ID DESC LIMIT 1");
+        $res['response']=$response;
+      
         echo json_encode( $res,0);
-      }
+        $conn->close;
+    }  
 ?>
